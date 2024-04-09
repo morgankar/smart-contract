792a693cfe4659507d1a30e461a56f8731e2b7f4
Rohan Kar <realrohankar03@gmail.com>
2024-04-09 20:19:28 +0530

    Added title variable for proposals

--- proposalContract.sol
+++ proposalContract.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 contract ProposalContract {
     struct Proposal {
+        string title;        //Proposal title
         string description;  //Description of the proposal
         uint256 approved;    //No. of approved votes
         uint256 rejected;    //No. of rejected votes
@@ -11,4 +12,7 @@ contract ProposalContract {
         bool currentState;  //Proposal passed or not
         bool canVote;       //If voting is still open
     }
+
+    mapping(uint256 => Proposal) proposal_history; //Records of previous proposals
+    
 }
