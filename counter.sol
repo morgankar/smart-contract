51b64df07372bccf8baa516a52f42611e37d4131
Rohan Kar <realrohankar03@gmail.com>
2024-04-09 19:55:19 +0530

    setting up makeProposal contract

--- counter.sol
+++ counter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 
-contract Example {
+contract ProposalContract {
 
 
     // This is the owner of the contract
---
+++ proposalContract.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.18;
+
+//Contract to make proposal with voting features 
+
+contract ProposalContract {
+    struct Proposal {
+        string description;  //Description of the proposal
+        uint256 approved;    //No. of approved votes
+        uint256 rejected;    //No. of rejected votes
+        uint256 votingLimit; 
+        bool currentState;  //Proposal passed or not
+        bool canVote;       //If voting is still open
+    }
+}
