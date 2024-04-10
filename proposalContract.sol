77c19e60da826261beb33cf2dfc3e83d261472e2
Rohan Kar <realrohankar03@gmail.com>
2024-04-10 21:40:11 +0530

    wip

--- proposalContract.sol
+++ proposalContract.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 //Contract to make proposal with voting features 
 
 contract ProposalContract {
+    uint256 private counter;  //assigning id for the counter
     struct Proposal {
         string title;        //Proposal title
         string description;  //Description of the proposal
@@ -14,5 +15,10 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history; //Records of previous proposals
+
+    function create(string calldata _description, uint256 _votingLimit) external {
+        counter +=1;
+        proposal_history[counter] = Proposal(_description, 0,0,0, votinglimit, false,true )
+    }
     
 }
